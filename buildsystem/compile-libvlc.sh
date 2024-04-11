067c239aedfd85e525bac68c036653d38a7bf76f
Steve Lhomme <robux4@ycbcr.xyz>
2024-04-11 13:54:06 +0200

    buildsystem: list the contrib packages to build
    So we can see what's going on. Other build targets do that too.

--- buildsystem/compile-libvlc.sh
+++ buildsystem/compile-libvlc.sh
@@ -502,6 +502,8 @@ if [ "$AVLC_USE_PREBUILT_CONTRIBS" -gt "0" ]; then
         make -C $VLC_CONTRIB_DIR prebuilt PREBUILT_URL="$VLC_PREBUILT_CONTRIBS_URL"
         avlc_checkfail "Fetching prebuilt contribs from ${VLC_PREBUILT_CONTRIBS_URL} failed"
     fi
+    # list packages to be built
+    make -C $VLC_CONTRIB_DIR TARBALLS="$VLC_TARBALLS" list
     # build native tools
     make -C $VLC_CONTRIB_DIR TARBALLS="$VLC_TARBALLS" tools
 else
@@ -522,6 +524,9 @@ else
 
     # fix modplug endianess check (narrowing error)
     export ac_cv_c_bigendian=no
+
+    # list packages to be built
+    make -C $VLC_CONTRIB_DIR TARBALLS="$VLC_TARBALLS" list
 
     make -C $VLC_CONTRIB_DIR TARBALLS="$VLC_TARBALLS" $MAKEFLAGS fetch
     avlc_checkfail "contribs: make fetch failed"
