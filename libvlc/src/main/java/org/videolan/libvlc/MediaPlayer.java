0e68d2511510acba70288222ff055db190b3df50
Steve Lhomme <robux4@ycbcr.xyz>
2024-09-04 10:16:55 +0200

    MediaPlayer: remove always true Lollipop (21) check

--- libvlc/src/main/java/org/videolan/libvlc/MediaPlayer.java
+++ libvlc/src/main/java/org/videolan/libvlc/MediaPlayer.java
@@ -512,7 +512,7 @@ public class MediaPlayer extends VLCObject<MediaPlayer.Event> {
     }
 
     private final BroadcastReceiver mAudioPlugReceiver =
-            AndroidUtil.isLolliPopOrLater && !AndroidUtil.isMarshMallowOrLater ? createAudioPlugReceiver() : null;
+            !AndroidUtil.isMarshMallowOrLater ? createAudioPlugReceiver() : null;
 
     private void registerAudioPlugV21(boolean register) {
         if (register) {
