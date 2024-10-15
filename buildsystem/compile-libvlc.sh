SteveParson/libvlcjni

commit ef560af54a24abc56ab8447b2f2d15e733d10e5b

buildsystem: call configure directly
Whether it fails or not, we can move between directories directly.

buildsystem/compile-libvlc.sh
@@ -567,7 +567,7 @@ if [ ! -e $VLC_BUILD_DIR/config.h -o "$AVLC_RELEASE" = 1 ]; then
         VLC_CONFIGURE_DEBUG="--enable-debug --disable-branch-protection"
     fi
 
-    (cd $VLC_BUILD_DIR && \
+    cd $VLC_BUILD_DIR
     CFLAGS="${VLC_CFLAGS}" \
     CXXFLAGS="${VLC_CFLAGS} ${VLC_CXXFLAGS}" \
     CC="${CROSS_CLANG}" \
@@ -580,11 +580,11 @@ if [ ! -e $VLC_BUILD_DIR/config.h -o "$AVLC_RELEASE" = 1 ]; then
     PKG_CONFIG_LIBDIR=$VLC_SRC_DIR/contrib/$TARGET_TUPLE/lib/pkgconfig \
     PKG_CONFIG_PATH=$VLC_SRC_DIR/contrib/$TARGET_TUPLE/lib/pkgconfig \
     PATH=../contrib/bin:$PATH \
-    sh ../configure --host=$TARGET_TUPLE --build=x86_64-unknown-linux \
+    ../configure --host=$TARGET_TUPLE --build=x86_64-unknown-linux \
         --with-contrib=${VLC_SRC_DIR}/contrib/${TARGET_TUPLE} \
         --prefix=${VLC_BUILD_DIR}/install/ \
-        ${EXTRA_PARAMS} ${VLC_CONFIGURE_ARGS} ${VLC_CONFIGURE_DEBUG} \
-    )
+        ${EXTRA_PARAMS} ${VLC_CONFIGURE_ARGS} ${VLC_CONFIGURE_DEBUG}
+    cd ..
     avlc_checkfail "vlc: configure failed"
 fi
 

commit ca18d383b967cdf75c62521acc70ebba8bbd2444

buildsystem: fix compilation when being called from any directory
ef560af54a24abc56ab8447b2f2d15e733d10e5b broke b6b33ffb7ee5306e6b99afde2be6fbd6cccdaa43

buildsystem/compile-libvlc.sh
@@ -560,6 +560,7 @@ if [ ! -e $VLC_BUILD_DIR/config.h -o "$AVLC_RELEASE" = 1 ]; then
         VLC_CONFIGURE_DEBUG="--enable-debug --disable-branch-protection"
     fi
 
+    BEFORE_VLC_BUILD_DIR=$(pwd -P)
     cd $VLC_BUILD_DIR
     CFLAGS="${VLC_CFLAGS}" \
     CXXFLAGS="${VLC_CFLAGS} ${VLC_CXXFLAGS}" \
@@ -575,7 +576,7 @@ if [ ! -e $VLC_BUILD_DIR/config.h -o "$AVLC_RELEASE" = 1 ]; then
         --with-contrib=${VLC_SRC_DIR}/contrib/${TARGET_TUPLE} \
         --prefix=${VLC_BUILD_DIR}/install/ \
         ${EXTRA_PARAMS} ${VLC_CONFIGURE_ARGS} ${VLC_CONFIGURE_DEBUG}
-    cd ..
+    cd "$BEFORE_VLC_BUILD_DIR"
     avlc_checkfail "vlc: configure failed"
 fi
 